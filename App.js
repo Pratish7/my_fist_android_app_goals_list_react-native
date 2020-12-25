901a0509d7cc24fb96bfddecd77baac86bfb02aa
Pratish7 <pratishbajpai7@gmail.com>
2020-12-25 22:18:38 +0530

    added useState, stored user input and console logged on onpress

--- App.js
+++ App.js
@@ -2,11 +2,27 @@ import React, { useState } from 'react';
 import { StyleSheet, TextInput, View, Button } from 'react-native'
 
 export default function App() {
+  
+  const [enteredGoal, setEneterdGoal] = useState('');
+  
+  const goalInputHandeler = (enteredText) => {
+    setEneterdGoal(enteredText);
+  }
+
+  const addGoalHandeler = () => {
+    console.log(enteredGoal);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} />
-        <Button title='ADD' />
+        <TextInput 
+          placeholder="Course Goal" 
+          style={styles.input} 
+          onChangeText={goalInputHandeler}
+          value={enteredGoal}
+        />
+        <Button title='ADD' onPress={addGoalHandeler} />
       </View>
       <View>
 
